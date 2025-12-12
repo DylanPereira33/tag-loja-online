8cc63a6395a3f27b742b7cbb6e6843e8727c069c
¨Dylan¨ <dylanfrancap@gmail.com>
2025-12-12 15:22:04 -0300

    Reformular a lógica de exibição e remoção do carrinho; adicionar aplicação de desconto para pagamentos em Pix.

--- src/main.py
+++ src/main.py
@@ -5,6 +5,17 @@ from funcoes_funcionais import calcular_total_carrinho, aplicar_desconto
 carrinho = Carrinho()
 cliente = Cliente("Joao das Coves", "joaodascoves@email.com")  # cliente fixo para simplificar
 
+def mostrar_carrinho():
+    print("\n--- Seu Carrinho ---")
+    if len(carrinho.itens) == 0:
+        print("Carrinho vazio.")
+    else:
+        for item in carrinho.itens:
+            print(f"{item.qtd}x {item.produto.get_nome()} - R${item.subtotal():.2f}")
+        total = calcular_total_carrinho(carrinho)
+        print(f"Total: R${total:.2f}")
+
+
 def menu():
     while True:
         print("\n=== Loja Online ===")
@@ -43,13 +54,17 @@ def menu():
 
                     else:
                         print("Opção inválida.")
+                        break
             case "3":
-                codigo = input("Digite o código do produto para remover: ")
-                if carrinho.remover(codigo):
-                    print("Produto removido!")
+                mostrar_carrinho()
+                if len(carrinho.itens) == 0:
+                    continue
                 else:
-                    print("Código não encontrado no carrinho.")
-
+                    codigo = input("Digite o código do produto para remover: ")
+                    if carrinho.remover(codigo):
+                        print("Produto removido!")
+                    else:
+                        print("Código não encontrado no carrinho.")
 
             case "4":
                 print("\n--- Seu Carrinho ---")
@@ -66,30 +81,37 @@ def menu():
 
             case "5":
                 total = calcular_total_carrinho(carrinho)
-                print("Aplicando desconto de 10%.")
-                desconto10 = aplicar_desconto(lambda v: v * 0.9)   # Uso real da função funcional aplicar_desconto
-                total = desconto10(total)
                 print(f"Valor final: R${total:.2f}")
                 print("Escolha forma de pagamento:")
+                print("Pagamentos com Pix têm 10% de desconto!")
                 print("1. Pix\n2. Cartão\n3. Boleto")
-                forma = int(input("Opção: "))
 
-                if forma == 1:
-                    pagamento = PagamentoPix()
-                elif forma == 2:
-                    pagamento = PagamentoCartao()
-                elif forma == 3:
-                    pagamento = PagamentoBoleto()
-                else:
+                forma = input("Opção: ")
+                if forma not in ["1", "2", "3"]:
                     print("Forma inválida!")
                     continue
 
+                forma = int(forma)
+
+                if forma == 1:
+                    pagamento = PagamentoPix()
+                    print("Aplicando desconto de 10%.")
+                    desconto10 = aplicar_desconto(lambda v: v * 0.9)
+                    total = desconto10(total)
+                    print(f"Total com desconto: R${total:.2f}")
+                elif forma == 2:
+                    pagamento = PagamentoCartao()
+                else:
+                    pagamento = PagamentoBoleto()
+
                 pedido = Pedido(cliente, carrinho, pagamento)
+                pedido.total = total
                 pedido.confirmar_pedido()
                 pedido.mostrar_resumo()
-
+                
                 # Limpa carrinho após finalizar
                 carrinho.limpar()
+
 
             case "6":
                 print("Saindo...")
